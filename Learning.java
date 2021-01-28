f768b918f7133101372dc9431669cf577ec2e6f2
Arcade Kwaku <arcade@avocet.io>
2021-01-28 21:48:23 +0000

    First commit

---
+++ Learning.java
@@ -0,0 +1,20 @@
+public class Learning {
+    public static void main(String[] args) {
+        int expression1 = 5 % 2 - (4 * 2 - 1);
+        System.out.println(expression1);
+
+        int expression2 = (3 + (2 * 2 - 5)) + 6 - 5;
+        // System.out.println(expression2);
+
+        int expression3 = 5 * 4 % 3 - 2 + 1;
+        // System.out.println(expression3);
+
+        /* Using the greater than or less than 
+        should print out a boolean answer
+        */
+        double creditsEarned = 176.5;
+        double creditsOfSeminar = 8;
+        double creditsToGraduate = 180;
+        System.out.println(creditsEarned > creditsToGraduate);
+    }
+} 
